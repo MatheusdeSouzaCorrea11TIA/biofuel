a83159ac171de58138fb3b8809b62e519bf87fa8
Matheus de Souza Correa - 31MATI <162612190+MatheusdeSouzaCorrea11TIA@users.noreply.github.com>
2026-08-26 07:44:37 -0300

    Mudanças em detalhes, fotos e video do jogo faltando

--- js/changeMode.js
+++ js/changeMode.js
@@ -22,15 +22,19 @@ function toggleModes() {
 function changeColors(darkmode) {
     const root = document.documentElement
 
+    //Ta preto
     if (darkmode) {
         root.style.setProperty('--white', '#FFFFFF')
         root.style.setProperty('--gray', '#A8A8A8')
         root.style.setProperty('--dark-gray', '#131212')
         root.style.setProperty('--black', '#000000')
+        root.style.setProperty('--input-bg', '#181818')
+    //Ta branco
     } else {
         root.style.setProperty('--white', '#000000')
         root.style.setProperty('--gray', '#585858')
         root.style.setProperty('--dark-gray', '#cfcece')
         root.style.setProperty('--black', '#FFFFFF')
+        root.style.setProperty('--input-bg', '#c5c5c5')
     }
 }
